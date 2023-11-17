bdb3762ed605e875f882eb52f7f2ad33d5ad26a0
Bertogim <91332073+Bertogim@users.noreply.github.com>
2023-11-17 21:53:12 +0100

    Optimized the page
    Added pages and a back button on the search midis

--- favorites.js
+++ favorites.js
@@ -2,6 +2,7 @@ const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 const fileListContainer = document.getElementById('file-list');
 const urlParams = new URLSearchParams(window.location.search);
+document.getElementById("BackButton").style.display = "none";
 
 // Obtener el término de búsqueda de los parámetros de la URL
 const searchTermFromURL = urlParams.get('search');
@@ -24,33 +25,85 @@ const favoriteFileNames = new Set(favorites.map(file => file.name));
 searchForm.addEventListener('submit', function (e) {
     e.preventDefault();
     const searchTerm = searchInput.value.toLowerCase();
-    fetchMidiFiles(searchTerm, favoriteFileNames);
+    fetchMidiFiles(searchTerm);
     // Actualizar la URL con el parámetro de búsqueda
     urlParams.set('search', searchTerm);
-     history.pushState(null, '', `?search=${encodeURIComponent(searchTerm)}`);
+    history.pushState(null, '', `?search=${encodeURIComponent(searchTerm)}`);
 });
 
-async function fetchMidiFiles(searchTerm = '') {
+let allMidiFiles = [];
+const pageSize = 50; // Número de MIDIs por página
+let currentPage = 1;
+
+async function fetchMidiFiles(searchTerm = '', page = 1, pageSize = 50) {
     try {
         const response = await fetch('https://api.github.com/repos/thewildwestmidis/midis/contents/');
         const data = await response.json();
 
         const midiFiles = data.filter(item => item.name.endsWith('.mid'));
-        const favoriteFileNames = new Set(favorites.map(file => file.name)); // Create favoriteFileNames here
+        const favoriteFileNames = new Set(favorites.map(file => file.name));
 
         // Filtrar por término de búsqueda si se proporciona
+        let filteredFiles = midiFiles;
         if (searchTerm) {
-            const filteredFiles = midiFiles.filter(file => file.name.toLowerCase().includes(searchTerm));
-            displayFileList(filteredFiles, favoriteFileNames); // Pass favoriteFileNames here
-        } else {
-            displayFileList(midiFiles, favoriteFileNames); // Pass favoriteFileNames here
+            filteredFiles = midiFiles.filter(file => file.name.toLowerCase().includes(searchTerm));
         }
+
+        // Filtrar por favoritos
+        filteredFiles = filteredFiles.filter(file => favoriteFileNames.has(file.name));
+
+        // Actualizar currentPage y pageSize globalmente
+        currentPage = page;
+
+        // Calcular límites de la página
+        const startIndex = (currentPage - 1) * pageSize;
+        const endIndex = startIndex + pageSize;
+
+        const paginatedFiles = filteredFiles.slice(startIndex, endIndex);
+
+        displayFileList(paginatedFiles);
+
+        // Generar la paginación
+        const totalPages = Math.ceil(filteredFiles.length / pageSize);
+        generatePagination(totalPages, currentPage, searchTerm);
     } catch (error) {
         console.error('Error fetching MIDI files:', error);
     }
 }
 
 
+function generatePagination(totalPages, currentPage, searchTerm) {
+    const paginationContainer = document.getElementById('pagination');
+    paginationContainer.innerHTML = '';
+
+    for (let i = 1; i <= totalPages; i++) {
+        const pageButton = document.createElement('button');
+        pageButton.textContent = i;
+        pageButton.addEventListener('click', () => {
+            fetchMidiFiles(searchTerm, i);
+            window.scrollTo(0, 0);
+        });
+
+        if (i === currentPage) {
+            pageButton.classList.add('active');
+        }
+
+        paginationContainer.appendChild(pageButton);
+    }
+
+    if (searchTerm) {
+        // Agregar botón de retroceso para búsquedas
+        const backButton = document.getElementById("BackButton")
+        backButton.style.display = "inline"
+        backButton.addEventListener('click', () => {
+            const searchInput = document.getElementById('search-input');
+            searchInput.value = '';
+            window.location.href = "/favorites";
+            fetchMidiFiles('', 1);
+            backButton.style.display = "none"
+        });
+    }
+}
 
 function formatFileName(name) {
     // Reemplazar "_" y "-" por " " (espacio)
@@ -84,14 +137,9 @@ async function displayFileList(files) {
             <button class="${isFavorite ? 'remove-favorite-button' : 'favorite-button'}" data-file='${JSON.stringify(file)}'>
                 ${isFavorite ? 'Unfavorite' : 'Favorite'}
             </button>
-
-            <!--
-            <button class="stop-button" style="display: none">Stop</button>
-            -->
-            `;
+        `;
 
         fileListContainer.appendChild(listItem);
-        
 
         // Cargar y mostrar la duración
         try {
@@ -116,11 +164,9 @@ async function displayFileList(files) {
                 localStorage.setItem(`midi_duration_${file.name}`, durationText);
             }
         } catch (error) {
-            console.error('Error loading duration of midi:', file.name, ' - ', error);
+            console.warn('Cant load duration of midi:', file.name, ' - ', error);
         }
-
     }});
-    
 
     const copyButtons = document.querySelectorAll('.copy-button');
     copyButtons.forEach(button => {
@@ -134,62 +180,23 @@ async function displayFileList(files) {
             }, 1000));
         });
     });
-    
+
     const playButtons = document.querySelectorAll('.play-button');
-    //const stopButtons = document.querySelectorAll('.stop-button');
 
     playButtons.forEach((playButton, index) => {
         playButton.addEventListener('click', async function () {
             const url = this.getAttribute('data-url');
             const midiplayer = createElementFromHTML('<midi-player class="Midi-player" sound-font visualizer="#myVisualizer"></midi-player>');
             midiplayer.setAttribute("src",url);
-            //midiplayer.style.display = 'none';
-            //midiplayer.setAttribute("sound-font visualizer","#section3 midi-visualizer");
             playButton.parentElement.getElementsByClassName("PlayMusicPos")[0].appendChild(midiplayer);
             playButton.remove()
 
-            //playButton.textContent = "Loading"
-            //playButton.classList.add('play-button-loading');
-            
             midiplayer.addEventListener('load', () => {
                 midiplayer.start();
-                            /*
-                playButton.textContent = "Play"
-                playButton.classList.remove('play-button-loading');
-                stopButtons.forEach(stopButton => {
-                    stopButton.style.display = 'none';
-                });
-                // Ocultar el botón "Play" actual
-                playButton.style.display = 'none';
-    
-                // Mostrar el botón "Stop" correspondiente
-                stopButtons[index].style.display = 'block';
-                // Mostrar el botón "Play" en los otros elementos
-                playButtons.forEach((button, idx) => {
-                    if (idx !== index) {
-                        button.style.display = 'block';
-                    }
-                });
-
-                // Parar el midi
-
-                const stopButton = playButton.parentElement.querySelector('.stop-button');
-
-                stopButton.addEventListener('click', () => {
-                    midiplayer.stop();
-                    midiplayer.remove();
-                    // Mostrar el botón "Play" en los otros elementos
-                    playButtons.forEach((button, idx) => {
-                        button.style.display = 'block';
-                    });
-                    // Esconder el botón "Stop" correspondiente
-                    stopButtons[index].style.display = 'none';
-        
-                });*/
             });
-        });            
+        });
     });
-    
+
     const favoriteButtons = document.querySelectorAll('.favorite-button');
     favoriteButtons.forEach(button => {
         button.addEventListener('click', function () {
@@ -245,27 +252,6 @@ function copyToClipboard(text) {
     document.execCommand('copy');
     document.body.removeChild(tempInput);
 }
-
-const favoriteButtons = document.querySelectorAll('.favorite-button');
-favoriteButtons.forEach(button => {
-    button.addEventListener('click', function () {
-        const fileData = JSON.parse(this.getAttribute('data-file'));
-        const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-
-        const existingIndex = favorites.findIndex(favorite => favorite.name === fileData.name);
-        if (existingIndex !== -1) {
-            // Already a favorite, remove it
-            favorites.splice(existingIndex, 1);
-            this.textContent = 'Favorite';
-        } else {
-            // Not a favorite, add it
-            favorites.push(fileData);
-            this.textContent = 'Unfavorite';
-        }
-
-        localStorage.setItem('favorites', JSON.stringify(favorites));
-    });
-});
 
 // Llamar a la función para obtener y mostrar la lista de archivos MIDI
 const searchTerm = urlParams.get('search');
